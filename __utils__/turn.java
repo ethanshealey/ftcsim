9865bb19139f597dbfc99ebda3375e2a0b0e9477
Ethan Shealey <ethan.shealey@gmail.com>
2025-04-15 21:45:58 -0400

    Update turn.java

--- __utils__/turn.java
+++ __utils__/turn.java
@@ -7,37 +7,37 @@
  */
 
 private void turnLeft() {
-  turnByAngel(-90.0);
+    turnByAngel(-90.0);
 }
 
 private void turnRight() {
-  turnByAngel(90.0);
+    turnByAngel(90.0);
 }
 
 private void turnByAngel(double angel) {
-  double currentAngel = imu.getAngularOrientation();
-  double targetAngel = currentAngel + angel;
-  
-  if(targetAngel < 0.0)
-      targetAngel = 360.0 - Math.abs(currentAngel + angel);
-  else if(targetAngel > 360.0)
-      targetAngel = targetAngel - 360.0;
-  
-  boolean shouldTurnLeft = shortestRotation(currentAngel, targetAngel) < 0;
-  
-  while(!((currentAngel > targetAngel - .1) && (currentAngel < targetAngel + .1))) {
-      motorLeft.setPower(shouldTurnLeft ? -.5 : .5);
-      motorRight.setPower(shouldTurnLeft ? .5 : -.5);
-      currentAngel = imu.getAngularOrientation();
-      
-      sleep(5);
-  }
-  
-  motorLeft.setPower(0);
-  motorRight.setPower(0);
+    double currentAngel = imu.getAngularOrientation();
+    double targetAngel = currentAngel + angel;
+    
+    if(targetAngel < 0.0)
+        targetAngel = 360.0 - Math.abs(currentAngel + angel);
+    else if(targetAngel > 360.0)
+        targetAngel = targetAngel - 360.0;
+    
+    boolean shouldTurnLeft = shortestRotation(currentAngel, targetAngel) < 0;
+    
+    motorLeft.setPower(shouldTurnLeft ? -.5 : .5);
+    motorRight.setPower(shouldTurnLeft ? .5 : -.5);
+    
+    while(!((currentAngel > targetAngel - .1) && (currentAngel < targetAngel + .1))) {
+        currentAngel = imu.getAngularOrientation();
+        sleep(2);
+    }
+    
+    motorLeft.setPower(0);
+    motorRight.setPower(0);
 }
 
 private int shortestRotation(int startAngle, int targetAngle) {
-  int delta = targetAngle - startAngle;
-  return ((delta + 180) % 360) - 180;
+    int delta = targetAngle - startAngle;
+    return ((delta + 180) % 360) - 180;
 }
